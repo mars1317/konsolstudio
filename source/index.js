64a779d24a3fba0d6806a3c1a8fa35961e907fb4
servatmandmariam1 <37312219+servatmandmariam1@users.noreply.github.com>
2020-09-10 21:18:45 +0300

    Add files via upload

---
+++ source/index.js
@@ -0,0 +1,6 @@
+import html from "./template/pages/index.html"
+import style from "./styles/main.scss";
+import script from "./scripts/main.js";
+
+import l from "./scripts/menuToggle";
+import slider from "./scripts/slider";
